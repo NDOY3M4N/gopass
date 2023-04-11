fe6cee9abeda240697c4845f49380a436e850db2
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-04-11 11:18:52 +0000

    ✅ :: add naive unit test for randomChar

---
+++ internal/randomChar_test.go
@@ -0,0 +1,46 @@
+package internal
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestRandomCharacter(t *testing.T) {
+	t.Run("get a random uppercase letter", func(t *testing.T) {
+		s := RandomUppercase()
+		assertCorrectRegex(t, s, "[A-Z]", "uppercase")
+	})
+
+	t.Run("get a random lowercase letter", func(t *testing.T) {
+		s := RandomLowercase()
+		assertCorrectRegex(t, s, "[a-z]", "lowercase")
+	})
+
+	t.Run("get a random number ", func(t *testing.T) {
+		s := RandomNumber()
+		assertCorrectRegex(t, s, "[0-9]", "number")
+	})
+
+	t.Run("get a random symbol", func(t *testing.T) {
+		s := RandomSymbol()
+
+		if !strings.Contains(symbols, s) {
+			t.Error("the random character is not a symbol")
+		}
+	})
+}
+
+func assertCorrectRegex(t testing.TB, character, pattern, charType string) {
+	t.Helper()
+
+	matched, err := regexp.MatchString(pattern, character)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if !matched {
+		t.Errorf("the random character is not a %s", charType)
+	}
+}
